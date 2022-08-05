67a890d9dbd3ba03f49ce0b1ff22003ded514378
m.pruglo <m.pruglo@cleack.com>
2022-08-05 12:12:18 +0300

    add mul

--- IntCode.py
+++ IntCode.py
@@ -7,3 +7,22 @@ class IntCode:
 
     def get_mem(self):
         return self.mem
+
+    def exec(self, start_address=0):
+        i = start_address
+        while True:
+            opcode = self.mem[i]
+            if opcode==99:
+                break
+
+            elif opcode==1:
+                a = self.mem[self.mem[i+1]]
+                b = self.mem[self.mem[i+2]]
+                self.mem[self.mem[i+3]] = a+b
+                i += 4
+
+            elif opcode==2:
+                a = self.mem[self.mem[i+1]]
+                b = self.mem[self.mem[i+2]]
+                self.mem[self.mem[i+3]] = a*b
+                i += 4
--- tests.py
+++ tests.py
@@ -12,6 +12,23 @@ class IntCodeTest(unittest.TestCase):
         ic.set_mem(mlist)
         self.assertEqual(ic.get_mem(), mlist)
 
+    def exec_check_mem(self, mem_before, mem_after):
+        ic = IntCode()
+        ic.set_mem(mem_before)
+        ic.exec()
+        self.assertEqual(ic.get_mem(), mem_after)
+
+    def test_opcode1(self):
+        self.exec_check_mem([1,0,0,0,99], [2,0,0,0,99])
+        self.exec_check_mem([1,1,1,4,99,5,6,0,99], [30,1,1,4,2,5,6,0,99])
+
+    def test_opcode2(self):
+        self.exec_check_mem([2,3,0,3,99], [2,3,0,6,99])
+        self.exec_check_mem([2,4,4,5,99,0], [2,4,4,5,99,9801])
+
+    def test_stop_at_99(self):
+        pass
+
 
 def run():
     unittest.main()
